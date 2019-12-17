e445b5ce6332c10f6b1673c03a0f7e6d010168f2
Chin Kuay <kuaychin@gmail.com>
2019-12-17 08:45:19 -0800

    Update BamazonCustomer.js

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -34,7 +34,6 @@ connection.query("SELECT * FROM Products", function(err, res){
   prompt.get(["buyItemQuantity"], function (err, result) {
   let buyItemQuantity = result.buyItemQuantity;
   console.log("You are buying " + buyItemQuantity + " units.");
-  
   connection.query('SELECT StockQuantity FROM Products WHERE ?', [{ItemID: buyItemID}], function(err, res){
   if(err) throw err;
   if(res[0] == undefined){
@@ -48,54 +47,19 @@ connection.query("SELECT * FROM Products", function(err, res){
   connection.query("UPDATE Products SET ? WHERE ?", [{StockQuantity: newInventory}, {ItemID: buyItemID}], function(err, res){
   if(err) throw err;
   });
-
-
-            // Show customer their purchase total (need to query the price info from database)
-            var customerTotal;
-            connection.query('SELECT Price FROM Products WHERE ?', [{ItemID: buyItemID}], function(err, res){
-              
-              var buyItemPrice = res[0].Price;
-              customerTotal = buyItemQuantity*buyItemPrice.toFixed(2);
-
-              console.log('\nYour total is $' + customerTotal + '.');
-
-              // ------------------------- Re factor for Executive Challenge ------------------------
-              // Find the department for the purchase item
-              connection.query('SELECT DepartmentName FROM Products WHERE ?', [{ItemID: buyItemID}], function(err, res){
-                var itemDepartment = res[0].DepartmentName;
-                
-                // Find the current Revenue for that department
-                connection.query('SELECT TotalSales FROM Departments WHERE ?', [{DepartmentName: itemDepartment}], function(err, res){
-                  var totalSales = res[0].TotalSales;
-
-                  // Calculate new sale revenue
-                  var totalSales = parseFloat(totalSales) + parseFloat(customerTotal);
-
-                  // Add the revenue from each transaction to the TotalSales column for the related department.
-                  connection.query('UPDATE Departments SET ? WHERE ?', [{TotalSales: totalSales}, {DepartmentName: itemDepartment}], function(err, res){
-                    if(err) throw err; // Error Handler
-                    console.log('Transaction Completed. Thank you!')
-                    connection.end(); // end the script/connection
-
-                  }); // end new revenue update query
-      
-                }); // end current revenue query
-
-              }); // end department name query 
-              // -------------------------------------------------------------------------------------
-            }); // end customer purchase update query 
-          }
-          // Insufficient inventory
-          else{
-            console.log('Sorry... We only have ' +  bamazonQuantity + ' of those items. Order cancelled.');
-            connection.end(); // end the script/connection
+  let customerTotal;
+  connection.query('SELECT Price FROM Products WHERE ?', [{ItemID: buyItemID}], function(err, res){            
+  let buyItemPrice = res[0].Price;
+  customerTotal = buyItemQuantity*buyItemPrice.toFixed(2);
+  console.log("Your total is $" + customerTotal);     
+  }); 
+  };
+  else{
+  console.log("Sorry... We only have " +  bamazonQuantity + " of those items. Order has been cancelled.");
+  connection.end();
           }
         }
-
-      }); // end item quantity query
-
-    }); // end of prompt 2
-
-  }); // end of prompt 1
-
-}); // end of main query
+      });
+    });
+  });
+});
